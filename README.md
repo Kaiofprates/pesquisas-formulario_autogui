347ec92f9e6a8559fc6401283deeb997b9145964
Kaiofprates <kaioprates@outlook.com>
2018-05-23 01:31:24 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Estudo de Formulário com Pyautogui
+
+![sqlite3](https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/SQLite370.svg/1200px-SQLite370.svg.png)
+
+> Estudo de Formulário, com integração com banco de dados sqlite e backup em txt. 
+
+> Requerimentos:
+
+*pyautogui
+*sqlite3
+
---
+++ formpy.py
@@ -0,0 +1,35 @@
+import pyautogui
+import sqlite3
+
+def sair():
+    cad = pyautogui.confirm(text='Deseja mesmo sair?',title='Formulário',buttons=['sim','não'])
+    if (cad == 'sim'):
+        pass
+    if (cad == 'não'):
+        pesq()
+
+def dados(nome,cidade,rua,numero,cep,sex):
+    cur = sqlite3.connect('data.db')
+    cursor = cur.cursor()
+    cursor.execute('create table if not exists users (id integer primary key autoincrement,nome text,cidade text, rua text, numero integer, cep integer, sex text);')
+    cursor.execute('insert into users (nome,cidade,rua,numero,cep,sex) values ("{}","{}","{}","{}","{}","{}"); '.format(nome,cidade,rua,numero,cep,sex))
+    cur.commit()
+    cur.close()
+
+def pesq():
+    cad = pyautogui.confirm(text = 'Deseja fazer um novo cadastro?', title = 'formulário',buttons=['sim','não'])
+    if (cad == 'não'):
+        sair()
+    if(cad == 'sim'):
+        nome = pyautogui.prompt(text = 'Nome',title = 'Formulário')
+        cidade = pyautogui.prompt(text = 'Cidade',title = 'Formulário',default='')
+        rua = pyautogui.prompt(text = 'Rua',title = 'Formulário',default='')
+        numero = pyautogui.prompt(text = 'Número',title = 'Formulário',default='')
+        cep = pyautogui.prompt(text = 'Cep',title = 'Formulário',default='')
+        sex = pyautogui.prompt(text = 'Sexo',title = 'Formulário',default='')
+        banco = open(nome+'.txt','w')
+        banco.write(nome+'\n'+cidade+'\n'+rua+'\n'+numero+'\n'+cep+'\n'+sex+'\n')
+        banco.close()
+        dados(nome,cidade,rua,numero,cep,sex)
+        print('Salvo com sucesso')
+pesq()
